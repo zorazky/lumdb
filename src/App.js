89f3a2bf5a6a8d5e3ff5207128415983678bc93b
Oswaldo <oswaldo.vazquez@gmail.com>
2017-11-18 22:46:57 -0600

    testing props and states

--- src/App.js
+++ src/App.js
@@ -3,18 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    toggle: true
+
+  }
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Welcome text="React is bitchin"/>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.toggle &&
+            <p>This should show and hide</p>
+        }
+
+        <button onClick={this.toggle}>Show / Hide</button>
       </div>
     );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    //take text property from props, to avoid writting this.props
+    const { text } = this.props;
+    return (
+      <h1 className="App-title">{text}</h1>
+    )
   }
 }
 
